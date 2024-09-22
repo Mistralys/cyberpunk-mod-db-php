75c8729e4680b3e78d0f49b70bd64cc2826642bc
Mistralys <s.mordziol@gmail.com>
2024-09-22 16:34:49 +0200

    Added data loaders; Added caching.

--- src/Mods/Collection/ModCollection.php
+++ src/Mods/Collection/ModCollection.php
@@ -13,7 +13,12 @@ use AppUtils\Collections\BaseStringPrimaryCollection;
 use AppUtils\FileHelper\FolderInfo;
 use AppUtils\FileHelper\JSONFile;
 use AppUtils\Interfaces\StringPrimaryRecordInterface;
+use Brick\Event\EventDispatcher;
 use CPMDB\Mods\Clothing\ClothingModInfo;
+use CPMDB\Mods\Collection\DataLoader\DataLoaderInterface;
+use CPMDB\Mods\Collection\DataLoader\Type\CacheDataLoader;
+use CPMDB\Mods\Collection\DataLoader\Type\FileDataLoader;
+use CPMDB\Mods\Collection\DataWriter\CacheDataWriter;
 use CPMDB\Mods\Mod\ModInfoInterface;
 
 /**
@@ -30,23 +35,43 @@ class ModCollection extends BaseStringPrimaryCollection
 {
     public const DB_GIT_NAME = 'mistralys/cyberpunk-mod-db';
 
+    public const EVENT_MODS_LOADED = 'ModsLoaded';
+
     private FolderInfo $dataFolder;
     private string $dataURL;
+    private FolderInfo $cacheFolder;
+    private DataLoaderInterface $dataLoader;
+    private EventDispatcher $events;
 
-    public function __construct(FolderInfo $dataFolder, string $dataURL)
+    public function __construct(FolderInfo $dataFolder, FolderInfo $cacheFolder, string $dataURL)
     {
         $this->dataFolder = $dataFolder;
+        $this->cacheFolder = $cacheFolder;
         $this->dataURL = $dataURL;
+        $this->events = new EventDispatcher();
+        $this->dataLoader = $this->createDataLoader();
     }
 
-    public static function create(FolderInfo $vendorFolder, string $vendorURL) : ModCollection
+    private function createDataLoader() : DataLoaderInterface
+    {
+        $registerCallback = $this->registerItem(...);
+
+        if(CacheDataWriter::cacheExists($this->cacheFolder) && CacheDataWriter::isCacheEnabled()) {
+            return new CacheDataLoader($this, $registerCallback, $this->cacheFolder);
+        }
+
+        return new FileDataLoader($this, $registerCallback);
+    }
+
+    public static function create(FolderInfo $vendorFolder, FolderInfo $cacheFolder, string $vendorURL) : ModCollection
     {
         return new ModCollection(
             FolderInfo::factory(sprintf(
-            '%s/%s/data',
-            $vendorFolder,
-            self::DB_GIT_NAME
+                '%s/%s/data',
+                $vendorFolder,
+                self::DB_GIT_NAME
             )),
+            $cacheFolder,
             sprintf(
                 '%s/%s/data',
                 $vendorURL,
@@ -55,9 +80,19 @@ class ModCollection extends BaseStringPrimaryCollection
         );
     }
 
+    public function isLoadedFromCache() : bool
+    {
+        return $this->dataLoader instanceof CacheDataLoader;
+    }
+
     public function getDataFolder() : FolderInfo
     {
         return $this->dataFolder;
+    }
+
+    public function getCacheFolder() : FolderInfo
+    {
+        return $this->cacheFolder;
     }
 
     public function getDataURL() : string
@@ -119,26 +154,47 @@ class ModCollection extends BaseStringPrimaryCollection
     protected function registerItems(): void
     {
         $this->registerCategoryMods($this->categoryClothing());
+
+        $this->triggerModsLoaded();
+    }
+
+    private function triggerModsLoaded() : void
+    {
+        $this->events->dispatch(self::EVENT_MODS_LOADED, $this);
+    }
+
+    /**
+     * Adds a listener for the mods loaded event, which is called
+     * once all mods have been loaded into memory.
+     *
+     * The callback gets a single parameter:
+     *
+     * - {@see ModCollection} this collection instance.
+     *
+     * @param callable $callback
+     * @return void
+     */
+    public function onModsLoaded(callable $callback) : void
+    {
+        $this->events->addListener(self::EVENT_MODS_LOADED, $callback);
     }
 
     private function registerCategoryMods(BaseCategory $category) : void
     {
-        $url = $category->getDataURL();
-        $folder = $category->getDataFolder();
-        $modClass = $category->getModClass();
-
-        foreach($category->getDataFiles() as $jsonFile) {
-            if($jsonFile instanceof JSONFile) {
-                $this->registerItem(ClassHelper::requireObjectInstanceOf(
-                    $modClass,
-                    new $modClass($category, $jsonFile, $folder, $url)
-                ));
-            }
-        }
+        $this->dataLoader->registerCategoryMods($category);
     }
 
     public function createFilter() : ModFilter
     {
         return new ModFilter($this);
+    }
+
+    public function writeCache() : void
+    {
+        if(CacheDataWriter::cacheExists($this->cacheFolder)) {
+            return;
+        }
+
+        (new CacheDataWriter($this, $this->cacheFolder))->write();
     }
 }
