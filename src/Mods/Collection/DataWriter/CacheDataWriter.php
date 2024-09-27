65dbd2a6a318f6f8c9101d71e6826208612a117b
Mistralys <s.mordziol@gmail.com>
2024-09-27 13:05:41 +0200

    Better update mechanism.

--- src/Mods/Collection/DataWriter/CacheDataWriter.php
+++ src/Mods/Collection/DataWriter/CacheDataWriter.php
@@ -4,14 +4,23 @@ declare(strict_types=1);
 
 namespace CPMDB\Mods\Collection\DataWriter;
 
+use AppUtils\BaseException;
+use AppUtils\FileHelper\FileInfo;
 use AppUtils\FileHelper\FolderInfo;
 use AppUtils\FileHelper\SerializedFile;
+use AppUtils\FileHelper_Exception;
 use CPMDB\Mods\Collection\ModCollection;
+use CPMDB\Mods\Collection\ModCollectionException;
 use CPMDB\Mods\Mod\ModInfoInterface;
+use Throwable;
 
 class CacheDataWriter
 {
+    public const ERROR_WRITING_CACHE_FAILED = 165401;
+
+    public const CACHING_SYSTEM_VERSION = '1';
     public const MODS_CACHE_FILE = 'mods.ser';
+    public const MODS_VERSION_FILE = 'mods.version';
 
     private ModCollection $collection;
 
@@ -22,10 +31,10 @@ class CacheDataWriter
     private FolderInfo $cacheFolder;
     private static bool $cacheEnabled = true;
 
-    public function __construct(ModCollection $collection, FolderInfo $cacheFolder)
+    public function __construct(ModCollection $collection)
     {
         $this->collection = $collection;
-        $this->cacheFolder = $cacheFolder;
+        $this->cacheFolder = $collection->getCacheFolder();
     }
 
     public static function isCacheEnabled() : bool
@@ -41,8 +50,16 @@ class CacheDataWriter
     public static function clearCache(FolderInfo $cacheFolder) : void
     {
         self::resolveCacheFile($cacheFolder)->delete();
+        self::resolveVersionFile($cacheFolder)->delete();
     }
 
+    /**
+     * Writes the mod data to the cache file, replacing the
+     * existing files if any.
+     *
+     * @return void
+     * @throws ModCollectionException
+     */
     public function write() : void
     {
         $this->workData = array();
@@ -51,14 +68,61 @@ class CacheDataWriter
             $this->collectMod($mod);
         }
 
-        self::resolveCacheFile($this->cacheFolder)->putData($this->workData);
+        try {
+            self::resolveCacheFile($this->cacheFolder)->putData($this->workData);
+            self::resolveVersionFile($this->cacheFolder)->putContents(self::resolveCacheVersion($this->collection));
+        }
+        catch (Throwable $e)
+        {
+            throw new ModCollectionException(
+                'Failed writing the mods cache data',
+                '',
+                self::ERROR_WRITING_CACHE_FAILED,
+                $e
+            );
+        }
 
         $this->workData = array();
     }
 
-    public static function cacheExists(FolderInfo $cacheFolder) : bool
+    /**
+     * Generates the cache version string based on the current
+     * database version, and the version of the caching system
+     * as defined in {@see self::CACHING_SYSTEM_VERSION}.
+     *
+     * @param ModCollection $collection
+     * @return string
+     *
+     * @throws BaseException
+     * @throws FileHelper_Exception
+     * @throws ModCollectionException
+     */
+    private static function resolveCacheVersion(ModCollection $collection) : string
     {
-        return self::resolveCacheFile($cacheFolder)->exists();
+        return 'Mods:v'.$collection->getDBVersion().';Cache:v'.self::CACHING_SYSTEM_VERSION;
+    }
+
+    /**
+     * Checks whether the mod data cache file exists, and is
+     * up to date with the current database version.
+     *
+     * @param ModCollection $collection
+     * @return bool
+     *
+     * @throws BaseException
+     * @throws FileHelper_Exception
+     */
+    public static function cacheIsValid(ModCollection $collection) : bool
+    {
+        $cacheFolder = $collection->getCacheFolder();
+        $versionFile = self::resolveVersionFile($cacheFolder);
+
+        return
+            self::resolveCacheFile($cacheFolder)->exists()
+            &&
+            $versionFile->exists()
+            &&
+            $versionFile->getContents() === self::resolveCacheVersion($collection);
     }
 
     public static function resolveCacheFile(FolderInfo $cacheFolder) : SerializedFile
@@ -67,6 +131,15 @@ class CacheDataWriter
             '%s/%s',
             $cacheFolder,
             self::MODS_CACHE_FILE
+        ));
+    }
+
+    public static function resolveVersionFile(FolderInfo $cacheFolder) : FileInfo
+    {
+        return FileInfo::factory(sprintf(
+            '%s/%s',
+            $cacheFolder,
+            self::MODS_VERSION_FILE
         ));
     }
 
