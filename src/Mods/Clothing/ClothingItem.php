87d6ddabd1202153d0362fd4a83de84ca03469f6
Mistralys <s.mordziol@gmail.com>
2024-09-27 18:14:43 +0200

    Updated to use the base item class.

--- src/Mods/Clothing/ClothingItem.php
+++ src/Mods/Clothing/ClothingItem.php
@@ -8,74 +8,22 @@ declare(strict_types=1);
 
 namespace CPMDB\Mods\Clothing;
 
-use AppUtils\ArrayDataCollection;
-use AppUtils\Interfaces\StringPrimaryRecordInterface;
-use CPMDB\Mods\Mod\ItemInfoInterface;
+use CPMDB\Mods\Items\BaseItem;
 
 /**
  * @package CPMDB
  * @subpackage Clothing Mods
+ *
+ * @method ClothingModInfo getMod()
  */
-class ClothingItem implements ItemInfoInterface
+class ClothingItem extends BaseItem
 {
-    private ArrayDataCollection $data;
-    private ClothingModInfo $mod;
-    private string $uuid;
-
     /**
      * @param ClothingModInfo $mod
      * @param array<string,mixed> $itemDef
      */
     public function __construct(ClothingModInfo $mod, array $itemDef)
     {
-        $this->mod = $mod;
-        $this->data = ArrayDataCollection::create($itemDef);
-        $this->uuid = $this->mod->getUUID().'.'.$this->getItemCode();
-    }
-
-    public function getID() : string
-    {
-        return $this->uuid;
-    }
-
-    public function getUUID(): string
-    {
-        return $this->uuid;
-    }
-
-    public function getMod(): ClothingModInfo
-    {
-        return $this->mod;
-    }
-
-    public function getModID() : string
-    {
-        return $this->mod->getUUID();
-    }
-
-    public function getName() : string
-    {
-        return $this->data->getString('name');
-    }
-
-    public function getNameWithCategory() : string
-    {
-        $category = $this->getCategory();
-
-        if(empty($category)) {
-            return $this->getName();
-        }
-
-        return $category.' - '.$this->getName();
-    }
-
-    public function getCategory() : string
-    {
-        return $this->data->getString('category');
-    }
-
-    public function getItemCode() : string
-    {
-        return $this->data->getString('code');
+        parent::__construct($mod, $itemDef);
     }
 }
