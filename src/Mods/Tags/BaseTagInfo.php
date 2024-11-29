d43e0e33f10c7047387951c25297a34475bd17a0
Mistralys <s.mordziol@gmail.com>
2024-11-29 14:39:07 +0100

    Added new methods.

--- src/Mods/Tags/BaseTagInfo.php
+++ src/Mods/Tags/BaseTagInfo.php
@@ -19,4 +19,66 @@ abstract class BaseTagInfo implements TagInfoInterface
     }
 
     abstract protected function _getName() : string;
+
+    /**
+     * @return string[]
+     */
+    public function getRelatedTagIDs() : array
+    {
+        return static::RELATED_TAGS;
+    }
+
+    public function hasRelatedTags() : bool
+    {
+        return !empty(static::RELATED_TAGS);
+    }
+
+    /**
+     * @var TagInfoInterface[]|null
+     */
+    private ?array $relatedTags = null;
+
+    /**
+     * Gets all tags related to this tag, if any.
+     * @return TagInfoInterface[]
+     */
+    public function getRelatedTags() : array
+    {
+        if(isset($this->relatedTags)) {
+            return $this->relatedTags;
+        }
+
+        $tags = array();
+        $collection = TagCollection::getInstance();
+
+        foreach($this->getRelatedTagIDs() as $tagID) {
+            $tags[] = $collection->getByID($tagID);
+        }
+
+        $this->relatedTags = $tags;
+
+        return $tags;
+    }
+
+    /**
+     * @return TagLink[]
+     */
+    abstract protected function _getLinks() : array;
+
+    /**
+     * @var TagLink[]|null
+     */
+    private ?array $links = null;
+
+    /**
+     * @return TagLink[]
+     */
+    public function getLinks() : array
+    {
+        if(!isset($this->links)) {
+            $this->links = $this->_getLinks();
+        }
+
+        return $this->links;
+    }
 }
--- src/Mods/Tags/TagInfoInterface.php
+++ src/Mods/Tags/TagInfoInterface.php
@@ -8,7 +8,28 @@ use AppUtils\Interfaces\StringPrimaryRecordInterface;
 
 interface TagInfoInterface extends StringPrimaryRecordInterface
 {
-    public function getLabel() : string;
+    public const RELATED_TAGS = array();
+
+    public function getDescription() : string;
 
     public function getCategory() : string;
+
+    public function hasRelatedTags() : bool;
+
+    /**
+     * @return string[]
+     */
+    public function getRelatedTagIDs() : array;
+
+    /**
+     * Gets all tags related to this tag, if any.
+     * @return TagInfoInterface[]
+     */
+    public function getRelatedTags() : array;
+
+    /**
+     * Gets Web links related to this tag, if any.
+     * @return TagLink[]
+     */
+    public function getLinks() : array;
 }
