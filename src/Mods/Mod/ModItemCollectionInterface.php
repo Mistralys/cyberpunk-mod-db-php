c9cfa694b6b4d7fa5674d545d197344f046ca043
Mistralys <s.mordziol@gmail.com>
2025-03-20 22:19:01 +0100

    Added category getter methods.

--- src/Mods/Mod/ModItemCollectionInterface.php
+++ src/Mods/Mod/ModItemCollectionInterface.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CPMDB\Mods\Mod;
 
+use CPMDB\Mods\Items\ItemCategory;
 use CPMDB\Mods\Items\ItemCollectionInterface;
 
 interface ModItemCollectionInterface extends ItemCollectionInterface
@@ -15,4 +16,30 @@ interface ModItemCollectionInterface extends ItemCollectionInterface
      * @return string
      */
     public function getCETCommands() : string;
+
+    /**
+     * Gets a category in the mod by its ID.
+     *
+     * > NOTE: Will throw an exception if the category
+     * > does not exist. Use {@see self::categoryExists()}
+     * > to check if a category exists.
+     *
+     * @param string $id
+     * @return ItemCategory
+     */
+    public function getCategoryByID(string $id) : ItemCategory;
+
+    /**
+     * Checks if a category exists in the mod.
+     *
+     * @param string $id
+     * @return bool
+     */
+    public function categoryExists(string $id) : bool;
+
+    /**
+     * Gets the IDs of all categories in the mod.
+     * @return string[]
+     */
+    public function getCategoryIDs() : array;
 }
