f3f3a345bd78cb4d10d504c8685f29b39b893272
Mistralys <s.mordziol@gmail.com>
2025-03-28 16:55:40 +0100

    Added a test.

--- tests/CPMDBTests/TestSuites/Collection/ClothingTests.php
+++ tests/CPMDBTests/TestSuites/Collection/ClothingTests.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CPMDBTests\TestSuites\Collection;
 
+use CPMDB\Mods\Mod\ModID;
 use CPMDBTEsts\TestClasses\CPMDBTestCase;
 
 final class ClothingTests extends CPMDBTestCase
@@ -20,5 +21,22 @@ final class ClothingTests extends CPMDBTestCase
         $this->assertSame($testID, $mod->getID());
         $this->assertStringContainsString('nexusmods.com', $mod->getAtelierURL());
         $this->assertTrue($mod->hasAtelier());
+    }
+
+    /**
+     * The test ID does not have the mod category,
+     * but thanks to {@see ModID}, it is still possible
+     * to retrieve the mod.
+     */
+    public function test_modIDexists() : void
+    {
+        $collection = $this->createCollection();
+        $testID = 'catsuit';
+
+        $this->assertTrue($collection->idExists($testID));
+
+        $mod = $collection->getByID($testID);
+
+        $this->assertSame($testID, $mod->getModID());
     }
 }
